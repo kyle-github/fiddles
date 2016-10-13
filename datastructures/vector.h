c72bf916678e1da64f5c712d16d600c30d62eafe
Kyle Hayes <kyle.hayes@gmail.com>
2016-10-12 17:16:54 -0700

    Fixes for basic tests.
    Typos and some small extra checks added based on tests.  Added implementation
    of vector delete function.

--- datastructures/vector.h
+++ datastructures/vector.h
@@ -26,10 +26,20 @@
 } T; \
 T *CONCAT(T,_create)(size_t capacity, size_t inc); \
 void CONCAT(T,_destroy)(T *vec); \
-size_t CONCAT(T,_size)(T *vec); \
-size_t CONCAT(T,_capacity)(T *vec); \
+static inline size_t CONCAT(T,_size)(T *vec) \
+{ \
+    if(vec) return vec->size; \
+    else return 0; \
+} \
+static inline size_t CONCAT(T,_capacity)(T *vec) \
+{ \
+    if(vec) return vec->capacity; \
+    else return 0; \
+} \
 size_t CONCAT(T,_splice)(T *vec, size_t loc, ELEM_TYPE *data, size_t data_len); \
-size_t CONCAT(T,_cut)(T *vec, size_t loc, size_t len);
+size_t CONCAT(T,_cut)(T *vec, size_t loc, size_t len); \
+static inline ELEM_TYPE CONCAT(T,_get)(T *vec, size_t loc) { return vec->elements[loc]; } \
+static inline size_t CONCAT(T,_set)(T *vec, size_t loc, ELEM_TYPE val) { vec->elements[loc] = (val); return loc; }
 
 
 
@@ -52,35 +62,41 @@ T *CONCAT(T,_create)(size_t capacity, size_t inc) \
     new_vec->inc = inc; \
     new_vec->size = 0; \
     return new_vec; \
-}\
-size_t CONCAT(T,_size)(T *vec) \
-{ \
-    if(vec) return vec->size; \
-    else return 0; \
 } \
-size_t CONCAT(T,_capacity)(T *vec) \
+void CONCAT(T,_destroy)(T *vec) \
 { \
-    if(vec) return vec->capacity; \
-    else return 0; \
+    if(!vec) return; \
+    if(vec->elements) free(vec->elements); \
+    free(vec); \
 } \
 size_t CONCAT(T,_splice)(T *vec, size_t loc, ELEM_TYPE *data, size_t data_len) \
 { \
-    size_t new_size = vec->size + data_len; \
     if(!vec) return 0; \
-    if(new_size > capacity) { \
+    size_t new_size = vec->size + data_len; \
+    if(new_size > vec->capacity) { \
         size_t new_cap = ((new_size + vec->inc - 1) / vec->inc) * vec->inc; \
         ELEM_TYPE *new_elements = calloc(new_cap, sizeof(ELEM_TYPE)); \
         if(!new_elements) return 0; \
+        for(size_t elem=0; elem < vec->size; elem++) { new_elements[elem] = vec->elements[elem]; } \
+        free(vec->elements); \
+        vec->elements = new_elements; \
         vec->capacity = new_cap; \
     } \
-    for(size_t elem = 0; elem < new_size; elem++) { \
-        if(elem < loc) { new_elements[elem] = vec->elements[elem]; } \
-        else if(elem < (loc+data_len) { new_elements[elem] = data[elem - loc]; } \
-        else { new_elements[elem] = vec->elements[elem - data_len]; } \
-    } \
-    free(vec->elements); \
-    vec->elements = new_elements; \
+    if(loc < vec->size) { for(size_t elem = vec->size - 1; elem >= loc; elem--) { vec->elements[elem+data_len] = vec->elements[elem]; } } \
+    for(size_t elem = 0; elem < data_len; elem++) { vec->elements[elem+loc] = data[elem]; } \
+    vec->size = new_size; \
     return loc + data_len; \
+} \
+size_t CONCAT(T,_cut)(T *vec, size_t loc, size_t len) \
+{ \
+    if(!vec) return 0; \
+    if(loc > vec->size) return 0; \
+    if((loc + len) > vec->size) { len = vec->size - loc; } \
+    for(size_t elem = loc + len; elem < vec->size; elem++) { \
+        vec->elements[elem - len] = vec->elements[elem]; \
+    } \
+    vec->size = vec->size - len; \
+    return loc; \
 }
 
 
--- datastructures/vector_test.c
+++ datastructures/vector_test.c
@@ -1,8 +1,41 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 #include "vector.h"
 
 VECTOR_DECL(int_vec, int);
 
-CONCAT(foo,_bar)
-
 VECTOR_IMPL(int_vec, int)
+
+
+void int_vec_print(int_vec *vec)
+{
+    printf("vec(%ld,%ld)[",int_vec_size(vec), int_vec_capacity(vec));
+    for(size_t elem = 0; elem < int_vec_size(vec); elem++) {
+        if(elem != 0) printf(", ");
+        printf("%d",int_vec_get(vec, elem));
+    }
+    printf("]\n");
+}
+
+int main()
+{
+    int_vec *vec = int_vec_create(4,10);
+    size_t index = 0;
+    int tmp = 0;
+    for(int i=0; i < 6; i++) {
+        index = int_vec_splice(vec, index, &i, (size_t)1);
+        int_vec_print(vec);
+    }
+    index = 4;
+    tmp = 15;
+    index = int_vec_splice(vec, index, &tmp, (size_t)1);
+    int_vec_print(vec);
+    index = int_vec_cut(vec, (size_t)2, (size_t)3);
+    int_vec_print(vec);
+
+    int_vec_destroy(vec);
+
+    return 0;
+}
 
