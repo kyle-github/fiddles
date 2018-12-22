621005e71a74f1081e2ed4e2130c4cc3e4ee24a4
Kyle Hayes <kyle.hayes@gmail.com>
2018-12-22 10:01:34 -0800

    Seems to be working.  Added more tests.   Changed popcount and find first functions.
    This is now more portable. The pop count function is done in standard C.  The
    find first function is done based on pop count.

--- bitvec/bitvec.c
+++ bitvec/bitvec.c
@@ -1,8 +1,9 @@
 
 #include <stdint.h>
-#include <string.h>
 #include <stdlib.h>
 #include "bitvec.h"
+
+#include <stdio.h>
 
 
 struct bitvec {
@@ -10,9 +11,20 @@ struct bitvec {
     uint64_t data[];
 };
 
+/* WARNING - this is not true when sizeof units are not 8-bit bytes! */
+static const int ELEM_BITS = (int)(sizeof(uint64_t)*8);
+static const uint64_t ALL_ONES = 0xFFFFFFFFFFFFFFFFL;
+
+
+
+static int popcount64(uint64_t val);
+static int find_first(uint64_t x);
+
+
+
 bitvec_p bitvec_create(int size)
 {
-    int actual_entries = (size + 63)/64;
+    int actual_entries = (size + (ELEM_BITS-1))/ELEM_BITS;
     bitvec_p result = NULL;
 
     if((result = calloc(1, sizeof(*result) + (actual_entries * sizeof(uint64_t)))) == NULL) {
@@ -46,74 +58,435 @@ int bitvec_get(bitvec_p bv, int index)
         return -1;
     }
 
-    mask = ((uint64_t)1 << (index % 64));
+    mask = ((uint64_t)1 << (index % ELEM_BITS));
 
-    return ((mask & bv->data[index/64]) ? 1 : 0);
+    return ((mask & bv->data[index/ELEM_BITS]) ? 1 : 0);
 }
 
 
 
-int bitvec_set(bitvec_p bv, int index, int val)
+int bitvec_set_range(bitvec_p bv, int start, int size)
 {
-    int result = 0;
-    uint64_t mask = 0;
+    int first_elem;
+    int last_elem;
+    uint64_t low_mask = 0;
+    uint64_t high_mask = 0;
 
-    result = bitvec_get(bv, index);
-
-    if(result < 0) {
-        return result;
+    if(!bv || start < 0 || size <= 0 || (start + size) > bv->size) {
+        return 0;
     }
 
-    mask = ((uint64_t)1 << (index % 64));
+    /*
+     * There are multiple cases to handle:
+     *
+     * 1) we are only setting one bit.
+     * 2) the range is within one word.
+     * 3) the range spans words.
+     */
 
-    if(val == 0) {
-        bv->data[index/64] &= (~mask);
-    } else {
-        bv->data[index/64] |= mask;
+    /* case 1: pull this out for speed as it is common. */
+    if(size == 1) {
+        uint64_t mask = ((uint64_t)1 << (start % ELEM_BITS));
+        bv->data[start/ELEM_BITS] |= mask;
+        return 1;
     }
 
-    return result;
+    first_elem = start/ELEM_BITS;
+    last_elem = (start + size - 1)/ELEM_BITS;
+
+    /* case 2 */
+    if(first_elem == last_elem) {
+        /*
+         * low mask:
+         *
+         * We want a mask that starts at the first bit to be set and goes
+         * up to the msb.
+         *
+         * first we set a single bit at the start of the range:
+         *     msb 0...0000100000...0 lsb
+         *                 ^
+         *                 +--- start
+         *
+         * Then we subtract one to get all ones below this:
+         *     msb 0...0000011111...1  lsb
+         *                 ^
+         *                 +--- start
+         *
+         * Then we invert it to get a mask that starts at the first bit and
+         * goes up to the top bit in the word.
+         *     msb 1...1111100000...0  lsb
+         *                 ^
+         *                 +--- start
+         *
+         * high mask:
+         *
+         * We want a mask that starts at the top bit to set and goes down
+         * to the lsb.
+         *
+         * We short circuit if the top bit to set is the top bit of the word.  In that
+         * case, the mask is all zero bits.
+         *
+         * If not, we set the bit at the top, this is just _past_ the top:
+         *
+         *     msb 0...0000100000...0 lsb
+         *                 ^^
+         *                 |+--- end
+         *                 +---- start + size
+         *
+         * Then we subtract one to get all ones below this:
+         *     msb 0...0000011111...1  lsb
+         *                 ^^
+         *                 |+--- end
+         *                 +---- start + size
+         *
+         * Then we AND the two masks.
+         *     msb 0...000111...111000...0 lsb
+         *                ^       ^
+         *                |       +--- start
+         *                +----------- end
+         */
+
+        low_mask = ~(((uint64_t)1 << (start % ELEM_BITS))-1);
+        high_mask = (((start + size) % ELEM_BITS) == 0 ? (uint64_t)0 : (((uint64_t)1 << ((start+size) % ELEM_BITS))-1));
+
+        bv->data[start/ELEM_BITS] |= (low_mask & high_mask);
+
+        return 1;
+    }
+
+    /* case 3 */
+
+    /*
+     * low mask:
+     *
+     * We want a mask that starts at the first bit to be set and goes
+     * up to the msb.
+     *
+     * first we set a single bit at the start of the range:
+     *     msb 0...0000100000...0 lsb
+     *                 ^
+     *                 +--- start
+     *
+     * Then we subtract one to get all ones below this:
+     *     msb 0...0000011111...1  lsb
+     *                 ^
+     *                 +--- start
+     *
+     * Then we invert it to get a mask that starts at the first bit and
+     * goes up to the top bit in the word.
+     *     msb 1...1111100000...0  lsb
+     *                 ^
+     *                 +--- start
+     *
+     * high mask:
+     *
+     * We want a mask that starts at the top bit to set and goes down
+     * to the lsb.
+     *
+     * We short circuit if the top bit to set is the top bit of the word.  In that
+     * case, the mask is all zero bits.
+     *
+     * If not, we set the bit at the top, this is just _past_ the top:
+     *
+     *     msb 0...0000100000...0 lsb
+     *                 ^^
+     *                 |+--- end
+     *                 +---- start + size
+     *
+     * Then we subtract one to get all ones below this:
+     *     msb 0...0000011111...1  lsb
+     *                 ^^
+     *                 |+--- end
+     *                 +---- start + size
+     *
+     * Then we AND the two masks.
+     *     msb 0...000111...111000...0 lsb
+     *                ^       ^
+     *                |       +--- start
+     *                +----------- end
+     *
+     * We set all the words in between to all ones.
+     */
+
+    low_mask = ~(((uint64_t)1 << (start % ELEM_BITS))-1);
+    high_mask = (((start + size) % ELEM_BITS) == 0 ? (uint64_t)0 : (((uint64_t)1 << ((start+size) % ELEM_BITS))-1));
+
+    /* loop over the words */
+    for(int index = first_elem; index <= last_elem; index++) {
+        if(index == first_elem) {
+            bv->data[index] |= low_mask;
+        } else if(index == last_elem) {
+            bv->data[index] |= high_mask;
+        } else {
+            bv->data[index] = ALL_ONES;
+        }
+    }
+
+    return 1;
+}
+
+
+int bitvec_clear_range(bitvec_p bv, int start, int size)
+{
+    int first_elem;
+    int last_elem;
+    uint64_t low_mask = 0;
+    uint64_t high_mask = 0;
+
+    if(!bv || start < 0 || size <= 0 || (start + size) > bv->size) {
+        return 0;
+    }
+
+    /*
+     * There are multiple cases to handle:
+     *
+     * 1) we are only clearing one bit.
+     * 2) the range is within one word.
+     * 3) the range spans words.
+     */
+
+    /* case 1: pull this out for speed as it is common. */
+    if(size == 1) {
+        uint64_t mask = ~((uint64_t)1 << (start % ELEM_BITS));
+        bv->data[start/ELEM_BITS] &= mask;
+        return 1;
+    }
+
+    first_elem = start/ELEM_BITS;
+    last_elem = (start + size - 1)/ELEM_BITS;
+
+    /* case 2 */
+    if(first_elem == last_elem) {
+        /*
+         * low mask:
+         *
+         * We want a mask that starts at the first bit to be set and goes
+         * up to the msb.
+         *
+         * first we set a single bit at the start of the range:
+         *     msb 0...0000100000...0 lsb
+         *                 ^
+         *                 +--- start
+         *
+         * Then we subtract one to get all ones below this:
+         *     msb 0...0000011111...1  lsb
+         *                 ^
+         *                 +--- start
+         *
+         * Then we invert it to get a mask that starts at the first bit and
+         * goes up to the top bit in the word.
+         *     msb 1...1111100000...0  lsb
+         *                 ^
+         *                 +--- start
+         *
+         * high mask:
+         *
+         * We want a mask that starts at the top bit to set and goes down
+         * to the lsb.
+         *
+         * We short circuit if the top bit to set is the top bit of the word.  In that
+         * case, the mask is all zero bits.
+         *
+         * If not, we set the bit at the top, this is just _past_ the top:
+         *
+         *     msb 0...0000100000...0 lsb
+         *                 ^^
+         *                 |+--- end
+         *                 +---- start + size
+         *
+         * Then we subtract one to get all ones below this:
+         *     msb 0...0000011111...1  lsb
+         *                 ^^
+         *                 |+--- end
+         *                 +---- start + size
+         *
+         * Then we AND the two masks.
+         *     msb 0...000111...111000...0 lsb
+         *                ^       ^
+         *                |       +--- start
+         *                +----------- end
+         *
+         * This is the range we want to _clear_, so we invert it.
+         *     msb 1...111000...000111...1 lsb
+         *                ^       ^
+         *                |       +--- start
+         *                +----------- end
+         *
+         */
+
+        low_mask = ~(((uint64_t)1 << (start % ELEM_BITS))-1);
+        high_mask = (((start + size) % ELEM_BITS) == 0 ? (uint64_t)0 : (((uint64_t)1 << ((start+size) % ELEM_BITS))-1));
+
+        bv->data[start/ELEM_BITS] &= ~(low_mask & high_mask);
+
+        return 1;
+    }
+
+    /* case 3 */
+
+    /*
+     * low mask:
+     *
+     * We want a mask that starts below the first bit to be set and goes
+     * down to the lsb.
+     *
+     * first we set a single bit at the start of the range:
+     *     msb 0...0000100000...0 lsb
+     *                 ^
+     *                 +--- start
+     *
+     * Then we subtract one to get all ones below this:
+     *     msb 0...0000011111...1  lsb
+     *                 ^
+     *                 +--- start
+     *
+     * high mask:
+     *
+     * We want a mask that starts above the high bit to set and goes up
+     * to the msb.
+     *
+     * We short circuit if the top bit to set is the top bit of the word.  In that
+     * case, the mask is all zero bits.
+     *
+     * If not, we set the bit at the top, this is just _past_ the top:
+     *
+     *     msb 0...0000100000...0 lsb
+     *                 ^^
+     *                 |+--- end
+     *                 +---- start + size
+     *
+     * Then we subtract one to get all ones below this:
+     *     msb 0...0000011111...1  lsb
+     *                 ^^
+     *                 |+--- end
+     *                 +---- start + size
+     *
+     * Then we invert it to get the upper mask.
+     *     msb 1...11111000000...0  lsb
+     *                 ^^
+     *                 |+--- end
+     *                 +---- start + size
+     *
+     * We set all the words in between to all zeros.
+     */
+
+    low_mask = (((uint64_t)1 << (start % ELEM_BITS))-1);
+    high_mask = (((start + size) % ELEM_BITS) == 0 ? (uint64_t)0 : ~(((uint64_t)1 << ((start+size) % ELEM_BITS))-1));
+
+    /* loop over the words */
+    for(int index = first_elem; index <= last_elem; index++) {
+        if(index == first_elem) {
+            bv->data[index] &= low_mask;
+        } else if(index == last_elem) {
+            bv->data[index] &= high_mask;
+        } else {
+            bv->data[index] = 0;
+        }
+    }
+
+    return 1;
 }
 
 
 
-void bitvec_clear(bitvec_p bv, int val)
+int bitvec_find_first(bitvec_p bv)
 {
-    int elem_count = 0;
-    uint64_t clear_val = (val ? 0xFFFFFFFFFFFFFFFF : 0x0000000000000000);
-
-    if(!bv) {
-        return;
-    }
-
-    elem_count = bv->size / 64;
-
-    for(int i=0; i < elem_count; i++) {
-        bv->data[i] = clear_val;
-    }
-}
-
-
-
-int bitvec_find_first(bitvec_p bv, int val)
-{
-    int elem_count = 0;
+    int num_elems = 0;
 
     if(!bv) {
         return -1;
     }
 
-    elem_count = bv->size / 64;
+    num_elems = bv->size / ELEM_BITS;
 
-    for(int i=0; i < elem_count; i++) {
-        uint64_t elem = (val ? bv->data[i] : ~(bv->data[i]));
-        int bit_index = ffsll(elem);
+    //printf("bitvec_find_first() num_elems = %d\n",num_elems);
 
-        if(bit_index != 0) {
-            return (i * 64) + (bit_index - 1); /* bit_index is 1-64, not 0-63.  0 means no bits set. */
+    for(int index=0; index < num_elems; index++) {
+        if(bv->data[index] != 0) {
+            //printf("bitvec_find_first() found first bit in element %d at position %d.\n", index, find_first(bv->data[index]));
+            return (index * ELEM_BITS) + find_first(bv->data[index]);
         }
     }
 
     return -1;
 }
 
+
+
+
+int bitvec_popcount_upto(bitvec_p bv, int top)
+{
+    int max_elem = 0;
+    int sum = 0;
+
+    if(!bv || top >= bv->size) {
+        return -1;
+    }
+
+    max_elem = top / ELEM_BITS;
+
+    //printf("max_elem = %d\n",max_elem);
+
+    for(int index = 0; index <= max_elem; index++) {
+        if(index != max_elem) {
+            sum += popcount64(bv->data[index]);
+        } else {
+            uint64_t mask = (((uint64_t)1 << (top % ELEM_BITS))-1);
+
+            //printf("mask = %lx & data = %lx\n", mask, bv->data[index]);
+
+            sum += popcount64(bv->data[index] & mask);
+        }
+    }
+
+    return sum;
+}
+
+
+
+/* helper functions */
+
+/*
+ * Algorithm from Wikipedia.
+ *
+ * This implements a population count/Hamming weight count for a 64-bit
+ * integer.  The assumption is that multiplication is fast.
+ */
+
+
+
+static const uint64_t m1  = 0x5555555555555555; //binary: 0101...
+static const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
+static const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+static const uint64_t m8  = 0x00ff00ff00ff00ff; //binary:  8 zeros,  8 ones ...
+static const uint64_t m16 = 0x0000ffff0000ffff; //binary: 16 zeros, 16 ones ...
+static const uint64_t m32 = 0x00000000ffffffff; //binary: 32 zeros, 32 ones
+static const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
+
+
+int popcount64(uint64_t x)
+{
+    if(x == ALL_ONES) {
+        return ELEM_BITS;
+    } else if(x == 0) {
+        return 0;
+    }
+
+    x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+    x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
+    x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
+    return (x * h01) >> 56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
+}
+
+
+/*
+ * Thanks to Wikipedia, we know that ffs(x) = pop(x ^ (~(−x))).
+ *
+ * But we want one less than that because we want to start indexes at zero.
+ */
+
+
+int find_first(uint64_t x)
+{
+    return popcount64(x ^ (~(-(int64_t)x)))-1;
+}
--- bitvec/main.c
+++ bitvec/main.c
@@ -2,35 +2,142 @@
 #include <stdio.h>
 #include "bitvec.h"
 
-#define BITVEC_SIZE (101)
+#define BITVEC_SIZE (300)
 
 int main(int argc, const char **argv)
 {
     bitvec_p bv = bitvec_create(BITVEC_SIZE);
-    int old_val = 0;
+    int status = 0;
+    int val = 0;
     int index = 0;
+    int count = 0;
 
+    printf("Creating bit vector.\n");
+    bv = bitvec_create(BITVEC_SIZE);
     assert(bv != NULL);
+    printf("OK\n");
 
     /* test == basic bit setting */
-    old_val = bitvec_set(bv, 40, 1);
-    assert(old_val == 0);
+    /* case 1 test. */
+    printf("Test setting single bit 40.\n");
+    status = bitvec_set_range(bv, 40, 1);
+    assert(status != 0);
+    printf("OK\n");
+
+    printf("Test individual bit 40\n");
+    val = bitvec_get(bv, 40);
+    assert(val == 1);
+    printf("OK\n");
 
     /* get bit finding */
-    index = bitvec_find_first(bv, 1);
+    printf("Test for first bit set is bit 40.\n");
+    index = bitvec_find_first(bv);
     assert(index == 40);
+    printf("OK\n");
 
-    old_val = bitvec_set(bv, 0, 1);
-    assert(old_val == 0);
+    printf("Test setting bit range in a word from bits 5-9 inclusive.\n");
+    status = bitvec_set_range(bv, 5, 5);
+    assert(status != 0);
+    printf("OK\n");
 
-    index = bitvec_find_first(bv, 1);
-    assert(index == 0);
+    printf("Test individual bit 0\n");
+    val = bitvec_get(bv, 0);
+    assert(val == 0);
+    printf("OK\n");
 
-    index = bitvec_find_first(bv, 0);
-    assert(index == 1);
+    printf("Test individual bit 1\n");
+    val = bitvec_get(bv, 1);
+    assert(val == 0);
+    printf("OK\n");
 
+    printf("Test individual bit 2\n");
+    val = bitvec_get(bv, 2);
+    assert(val == 0);
+    printf("OK\n");
+
+    printf("Test individual bit 3\n");
+    val = bitvec_get(bv, 3);
+    assert(val == 0);
+    printf("OK\n");
+
+    printf("Test individual bit 4\n");
+    val = bitvec_get(bv, 4);
+    assert(val == 0);
+    printf("OK\n");
+
+    printf("Test individual bit 5\n");
+    val = bitvec_get(bv, 5);
+    assert(val == 1);
+    printf("OK\n");
+
+    printf("Test individual bit 6\n");
+    val = bitvec_get(bv, 6);
+    assert(val == 1);
+    printf("OK\n");
+
+    printf("Test individual bit 7\n");
+    val = bitvec_get(bv, 7);
+    assert(val == 1);
+    printf("OK\n");
+
+    printf("Test individual bit 8\n");
+    val = bitvec_get(bv, 8);
+    assert(val == 1);
+    printf("OK\n");
+
+    printf("Test individual bit 9\n");
+    val = bitvec_get(bv, 9);
+    assert(val == 1);
+    printf("OK\n");
+
+    printf("Test individual bit 10\n");
+    val = bitvec_get(bv, 10);
+    assert(val == 0);
+    printf("OK\n");
+
+    printf("Test for first bit set is bit 5.\n");
+    index = bitvec_find_first(bv);
+    assert(index == 5);
+    printf("OK\n");
+
+    printf("Test popcount up to 5.\n");
+    count = bitvec_popcount_upto(bv, 5);
+    assert(count == 0);
+    printf("OK\n");
+
+    printf("Test popcount up to 10.\n");
+    count = bitvec_popcount_upto(bv, 10);
+    assert(count == 5);
+    printf("OK\n");
+
+    printf("Test popcount up to 100.\n");
+    count = bitvec_popcount_upto(bv, 100);
+    assert(count == 6);
+    printf("OK\n");
+
+    printf("Test setting bit range in a word from bits 31-33 inclusive.\n");
+    status = bitvec_set_range(bv, 31, 3);
+    assert(status != 0);
+    printf("OK\n");
+
+    printf("Test popcount up to 100.\n");
+    count = bitvec_popcount_upto(bv, 100);
+    assert(count == 9);
+    printf("OK\n");
+
+    /* case 3 of set */
+    printf("Test setting bit range across multiple words from bit 16 up to bit 144.\n");
+    status = bitvec_set_range(bv, 16, 128);
+    assert(status != 0);
+    printf("OK\n");
+
+    printf("Test popcount up to 150.\n");
+    count = bitvec_popcount_upto(bv, 150);
+    assert(count == 133);
+    printf("OK\n");
 
     bitvec_destroy(bv);
+    printf("All OK\n");
 
     return 0;
 }
